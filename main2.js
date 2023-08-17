639f9c82c0dc6b8d7201bb14ecf67b66975d6715
S <52811487+sholpan4@users.noreply.github.com>
2023-08-17 21:46:58 +0600

    1
    1

--- main2.js
+++ main2.js
@@ -1,3 +1,6 @@
 let myVar = prompt('Please enter number: ');
-const result = myVar ** 2
+let myVar2 = prompt('Please enter number: ');
+// const result = myVar ** 2
+// const result = Math.pow(5, 2)
+const result = (myVar + myVar2) / 2
 alert(result)
