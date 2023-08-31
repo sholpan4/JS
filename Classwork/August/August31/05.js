a38440b8f300a620efc5dfcd679c2a28d1816768
S <52811487+sholpan4@users.noreply.github.com>
2023-08-31 21:32:02 +0600

    31
    31

--- Classwork/August/August31/05.js
+++ Classwork/August/August31/05.js
@@ -1,16 +1,29 @@
 function getIndexOfSymbol(str) {
-    let strSplit = str.split(' ');
-    index = [];
-
-    for (let i of strSplit) {
+    let strCounter = 0
+    for (let item of str){
+            console.log("element " + item);
+        }
         
-        index += str.indexOf(i)
-    }
+        for (let index in str){
+            console.log("index " + index);
+        }
+        
+        for (let i  = 0; i < str.length; i++){
+            if (i == i++) {
+                strCounter += 1
+                console.log("index: " + i + " element: " + str[i]);
+                console.log(strCounter)
 
-    return index
+
+
+            }
+            
+        }
+        
 }
 
-str = `dragon`
+str = `dragonjjj`
 // index = str.indexOf()
 
 console.log(getIndexOfSymbol(str))
+console.log(strCounter)
