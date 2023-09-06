5e0dd8fe3e018e5d3478f5d607e9ce6d88e90111
sholpan4 <b.o.sholpan44@gmail.com>
2023-09-06 11:06:13 +0600

    31
    31

---
+++ Homework JS/August31/task1.js
@@ -0,0 +1,4 @@
+const str = "sdfASfsAFgbd1232b3b2j3b5b23hb5";
+    const sumNumbers = str.match(/\d+/g).join('').length;
+    const sumLetters = str.match(/[a-z]/gi).join('').length;
+    console.log(sumNumbers, sumLetters);
