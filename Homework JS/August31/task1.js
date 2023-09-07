b43895f1bf6967b00044a5e09c568e785e97ac92
S <52811487+sholpan4@users.noreply.github.com>
2023-09-07 18:30:42 +0600

    Update task1.js
    *

--- Homework JS/August31/task1.js
+++ Homework JS/August31/task1.js
@@ -1,4 +1,5 @@
 const str = "sdfASfsAFgbd1232b3b2j3b5b23hb5";
-    const sumNumbers = str.match(/\d+/g).join('').length;
-    const sumLetters = str.match(/[a-z]/gi).join('').length;
-    console.log(sumNumbers, sumLetters);
+
+const sumNumbers = str.match(/\d+/g).join('').length;
+const sumLetters = str.match(/[a-z]/gi).join('').length;
+console.log(sumNumbers, sumLetters);
