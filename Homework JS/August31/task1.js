8eebd0b3324e69006424425535d26fd3aa6ae861
sholpan4 <b.o.sholpan44@gmail.com>
2023-09-14 16:34:07 +0600

    1
    1

--- Homework JS/August31/task1.js
+++ Homework JS/August31/task1.js
@@ -1,5 +1,48 @@
-const str = "sdfASfsAFgbd1232b3b2j3b5b23hb5";
+const str = ("sdf!#hjh12321sda11wwj jkdjkas$jl1");
 
-const sumNumbers = str.match(/\d+/g).join('').length;
-const sumLetters = str.match(/[a-z]/gi).join('').length;
-console.log(sumNumbers, sumLetters);
+function getSumNumbers(str) {
+    let sumNumbers = str.match(/\d+/g).join('').length;
+    return sumNumbers 
+}
+
+function getSumLetters(str){
+    let sumLetters = str.match(/[a-z]/gi).join('').length;
+    return sumLetters
+}
+
+// function getSumSign(str){
+//     const sumSigns = str.match(/...../gi).join('').length;
+//     return sumSigns
+// }
+
+    console.log('Количество цифр:',  getSumNumbers(str));
+    console.log('Количество букв:', getSumLetters(str));
+    // console.log('Количество знаков:', getSumSign(str))
+
+
+// function symbolAll (str) {
+//     str = str.split(' ').join('');
+//     console.log(str)
+
+//     let sumNumbers = '';
+//     let sumLetters = '';
+//     let sumSigns = '';
+
+//     for (const character of str){
+//         if (parseIn(character)) {
+//             sumNumbers += character;
+//         }
+//         else if (/[a-z]/i.test(character)){
+//             sumLetters += character
+//         }
+//         else {
+//             sumSigns += character
+//         }
+//     }
+
+//     console.log('Количество цифр:', sumNumbers)
+//     console.log('Количество букв:', sumLetters)
+//     console.log('Количество символы:', sumSigns)
+// }
+
+// symbolAll('sdf!"№jhjh12321sda11wwj jkdjkasjl1')
