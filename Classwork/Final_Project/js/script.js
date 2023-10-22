3805153b9cd018bf7ad5953b3fe56ee33e0d2eac
S <52811487+sholpan4@users.noreply.github.com>
2023-10-22 16:06:24 +0600

    `
    `

--- Classwork/Final_Project/js/script.js
+++ Classwork/Final_Project/js/script.js
@@ -1,13 +1,29 @@
-window.onload = () => {
-    let books = [
-        {
-            id: 0,
+// window.onload = () => {
+//     let books = [
+//         {
+//             id: 0,
 
-        },
-    ]
+//         },
+//     ]
 
-    localStorage.setItem("books", JSON.stringify(books));
-    console.log(localStorage.getItem('books'));
-}
+//     localStorage.setItem("books", JSON.stringify(books));
+//     console.log(localStorage.getItem('books'));
+// }
 
-const addVisitorButton
+// const editModal = document.getElementById('edit-modal');
+// const add = document.querySelector('add')
+
+
+
+
+
+
+
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
